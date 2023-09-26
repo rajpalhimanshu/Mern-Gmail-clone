0ff960d34d6cad25bc45c5b2c8f5aefdad68f71e
rajpalhimanshu <rajpalhimanshu557@gmail.com>
2023-09-26 23:01:36 +0530

    added changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/', routes);
 app.use(express.static(path.join(__dirname,"./client/build")));
 
 app.get('*',function(_,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.js"),function(err){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"),function(err){
       res.status(500).send(err);  
     })
 })
